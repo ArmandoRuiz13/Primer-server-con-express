6da2b147dab579bfafedee191785654be8adceb3
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-25 19:29:41 -0500

    5- agregando Query params, finalizando mini proyecto

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.get('/explorersInNode',(req,res) => {
     const explorer = {name: "Explorer", msg: "Hellow"}
     res.send(explorer)
 })
-
+app.get('/explorers/:explorerName', (req,res) => {
+    res.send(req.params)
+})
 app.listen(port,() => {
     console.log("Server Listo!")
 })
